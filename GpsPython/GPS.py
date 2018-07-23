f2903a1b2165dd38ff861a04614ddcdcd63d2d25
Tamata Raspi <contact@tamataocean.com>
2018-07-23 16:45:31 +0200

    Update - Try Catch control on GPS data

--- GpsPython/GPS.py
+++ GpsPython/GPS.py
@@ -1,12 +1,14 @@
 import time
-
 import serial
 import os
+import sys
+
 from datetime import datetime
 
 firstFixFlag = False # this will go true after the first GPS fix.
 firstFixDate = ""
-DEBUG = True
+DEBUG = False
+SLEEP = 10
 
 # Set up serial:
 ser = serial.Serial(
@@ -15,7 +17,7 @@ ser = serial.Serial(
     parity=serial.PARITY_NONE,\
     stopbits=serial.STOPBITS_ONE,\
     bytesize=serial.EIGHTBITS,\
-        timeout=1)
+    timeout=1)
 
 # Helper function to take HHMM.SS, Hemisphere and make it decimal:
 def degrees_to_decimal(data, hemisphere):
@@ -55,33 +57,35 @@ def parse_GPRMC(data):
     return dict
 
 # Main program loop:
+print "Start : %s" % time.ctime()
 while True:
-    print "Start : %s" % time.ctime()
+    
     line = ser.readline()
     if "$GPRMC" in line: # This will exclude other NMEA sentences the GPS unit provides.
         if DEBUG is True: 
-            print "GPRMC entering..." 
-        gpsData = parse_GPRMC(line) # Turn a GPRMC sentence into a Python dictionary called gpsData
+            print "GPRMC entering..." + line
+        
+        try:    
+            gpsData = parse_GPRMC(line) # Turn a GPRMC sentence into a Python dictionary called gpsData
+        
+        except parseerror as error:
+            print "!!! Error catched... stopping program "
+            print "!!! -------------------------------- !!!! "
+            print "!!! Parsing GPRMC error : " + error
+            print "Ending at : %s" % time.ctime()
+            sys.exit(0)
+            
+
         if gpsData['validity'] == "A": # If the sentence shows that there's a fix, then we can log the line
             if DEBUG is True: 
-                print "GpsData validity entering..." 
+                print "Writing into database..." 
+            
+            cus_date = datetime.strptime(gpsData['fix_date'], "%d%m%Y").date()                
+            
+            # Data to insert 
+            os.system("docker exec -u postgres pi_postgis_1 psql tamatatracking -c " +
+                "\"insert into trame ( jour, heure, latitude, longitude, geom ) " + 
+                "values ( current_date ,'"+ gpsData['fix_time']+"',"+str(gpsData['decimal_latitude'])+","+ str(gpsData['decimal_longitude'])+",st_setsrid( st_makepoint( "+ str( gpsData['decimal_longitude']) +","+ str(gpsData['decimal_latitude']) +" ), 4326 ) );\" ")
+            time.sleep( SLEEP )
 
-            if firstFixFlag is False: # If we haven't found a fix before, then set the filename prefix with GPS date & time.
-                # printf("Date = %s", gpsData['fix_date'])
-                # printf("Time = %d", gpsData['fix_time'])
-
-                firstFixDate = gpsData['fix_date'] + "-" + gpsData['fix_time']
-                firstFixFlag = True
-            else: # write the data to a simple log file and then the raw data as well:
-                if DEBUG is True: 
-                    print "Writing into database..." 
-                
-                cus_date = datetime.strptime(gpsData['fix_date'], "%d%m%Y").date()                
-                
-                # Data to insert 
-                os.system("docker exec -u postgres pi_postgis_1 psql tamatatracking -c " +
-                    "\"insert into trame ( jour, heure, latitude, longitude, geom ) " + 
-                    "values ( current_date ,'"+ gpsData['fix_time']+"',"+str(gpsData['decimal_latitude'])+","+ str(gpsData['decimal_longitude'])+",st_setsrid( st_makepoint( "+ str( gpsData['decimal_longitude']) +","+ str(gpsData['decimal_latitude']) +" ), 4326 ) );\" ")
-                time.sleep( 10 )
-    
     
